2c01ed2853a6d249e84fcccd2c4c11a237370991
enyan27 <truongdat2325@gmail.com>
2026-02-09 13:43:06 +0700

    feat: add bookmark-saver

---
+++ 5-bookmark-saver/script.js
@@ -0,0 +1,68 @@
+const addBookmarkBtn = document.getElementById("add-bookmark");
+const bookmarkList = document.getElementById("bookmark-list");
+const bookmarkNameInput = document.getElementById("bookmark-name");
+const bookmarkUrlInput = document.getElementById("bookmark-url");
+
+document.addEventListener("DOMContentLoaded", loadBookmarks);
+
+addBookmarkBtn.addEventListener("click", function () {
+    const name = bookmarkNameInput.value.trim();
+    const url = bookmarkUrlInput.value.trim();
+
+    if (!name || !url) {
+        alert("Vui lòng nhập cả Name và URL của bookmark.");
+        return;
+    } else {
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            alert("Vui lòng nhập URL hợp lệ. VD: http:// hoặc https://");
+            return;
+        }
+
+        addBookmark(name, url);
+        saveBookmark(name, url);
+        bookmarkNameInput.value = "";
+        bookmarkUrlInput.value = "";
+    }
+});
+
+function addBookmark(name, url) {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = url;
+    link.textContent = name;
+    link.target = "_blank";
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.addEventListener("click", function () {
+        bookmarkList.removeChild(li);
+        removeBookmarkFromStorage(name, url);
+    });
+
+    li.appendChild(link);
+    li.appendChild(removeButton);
+
+    bookmarkList.appendChild(li);
+}
+
+function getBookmarksFromStorage() {
+    const bookmarks = localStorage.getItem("bookmarks");
+    return bookmarks ? JSON.parse(bookmarks) : [];
+}
+
+function saveBookmark(name, url) {
+    const bookmarks = getBookmarksFromStorage();
+    bookmarks.push({ name, url });
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
+
+function loadBookmarks() {
+    const bookmarks = getBookmarksFromStorage();
+    bookmarks.forEach((bookmark) => addBookmark(bookmark.name, bookmark.url));
+}
+
+function removeBookmarkFromStorage(name, url) {
+    let bookmarks = getBookmarksFromStorage();
+    bookmarks = bookmarks.filter((bookmark) => bookmark.name !== name || bookmark.url !== url);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+}
